fc0731dcafaa7be9d082677c0759a6b7bc9e0802
Aaron Killen <akillen@gmail.com>
2014-09-06 17:30:39 -0400

    A bit of cleanup and intermediary code organization.

--- game.js
+++ game.js
@@ -4,6 +4,9 @@ canvas.width = 512;
 canvas.height = 480;
 document.body.appendChild(canvas);
 
+
+//Load graphic resources
+
 var bgReady = false;
 var bgImage = new Image();
 bgImage.onload = function() {
@@ -32,41 +35,55 @@ bulletImage.onload = function(){
 }
 bulletImage.src = "images/bullet.png";
 
+
+//Constants
+
 var direction = Object.freeze({UP: "up", DOWN: "down", LEFT: "left", RIGHT: "right"});
-
-var shotDelay = 300;
-var lastShot = Date.now() - shotDelay;
-
-
-var hero = {
-	speed: 256,
-	x: 0,
-	y: 0,
-	shotDelay: 300,
-	lastShot: Date.now() - this.shotDelay,
-	readyToShoot: function() {
-		return Date.now() - this.lastShot > shotDelay;
-	},
-	shoot: function (bulletDir) {
-		if (this.readyToShoot()){
-			bullets[bullets.length] = {dir: bulletDir, x: this.x + 8, y: this.y + 8};
-			this.lastShot = Date.now();
-		}
-	}
-};
-
-var monster = {
-	x: 0,
-	y: 0
-};
-
-var monstersCaught = 0;
-
-var bullets = [];
-
 var moveKeys = [87, 83, 65, 68];
 var shootKeys = [37, 38, 39, 40];
 
+//Classes
+
+function Hero() {
+	this.health = 4;
+	this.maxspeed = 256;
+	this.speed = 0;
+	this.x = 0;
+	this.y = 0;
+	var shotDelay = 300;
+	var lastShot = Date.now() - shotDelay;
+	this.readyToShoot = function() {
+		return Date.now() - lastShot > shotDelay;
+	};
+	this.shoot = function (bulletCollection, bulletDir) {
+		if (this.readyToShoot()){
+			bulletCollection[bulletCollection.length] = {dir: bulletDir, x: this.x + 8, y: this.y + 8};
+			lastShot = Date.now();
+		}
+	};
+	this.render = function (context) {
+		if (heroReady) {
+			context.drawImage(heroImage, this.x, this.y);
+		}
+	};
+	this.accelerate = function(delta) {
+		if (this.speed < this.maxspeed) {
+			this.speed += delta * 800;
+		}
+	};
+};
+
+function Monster() {
+	this.x = 0;
+	this.y = 0;
+}
+
+//Game state objects
+var hero = new Hero();
+var monster = new Monster();
+var monstersCaught = 0;
+var heroInvulnTime = 0;
+var bullets = [];
 var keysDown = {};
 
 addEventListener("keydown", function (e) {
@@ -83,6 +100,8 @@ var reset = function () {
 	monster.y = 32 + (Math.random() * (canvas.height - 64));
 };
 
+//Evalute each bullet for collision, remove if collided
+//TODO: Refactor collision logic to common function
 var bulletCollision = function() {
 	for (var i = 0; i < bullets.length; i++){
 		if (
@@ -105,32 +124,44 @@ var addBullet = function (flyDirection, x, y) {
 //Update game objects
 var update = function (modifier) {
 	//Movement
+	var heroMoving = false;
 	if (87 in keysDown && hero.y > 0) { //W
 		hero.y -= hero.speed * modifier;
+		heroMoving = true;
 	}
 	if (83 in keysDown && hero.y < canvas.width - 64) { //S
 		hero.y += hero.speed * modifier;
+		heroMoving = true;
 	}
 	if (65 in keysDown && hero.x > 0) { //A
 		hero.x -= hero.speed * modifier;
+		heroMoving = true;
 	}
 	if (68 in keysDown && hero.x < canvas.width - 32) { //D
 		hero.x += hero.speed * modifier;
+		heroMoving = true;
 	}
+	if (heroMoving) { 
+		hero.accelerate(modifier);
+	} else {
+		hero.speed = 0;
+	}
+
 
 	//Shoot keys
 	if (38 in keysDown) { // Player holding up
-		hero.shoot(direction.UP);
+		hero.shoot(bullets, direction.UP);
 	}
 	if (40 in keysDown) { // Player holding down
-		hero.shoot(direction.DOWN);
+		hero.shoot(bullets, direction.DOWN);
 	}
 	if (37 in keysDown) { // Player holding left
-		hero.shoot(direction.LEFT);
+		hero.shoot(bullets, direction.LEFT);
 	}
 	if (39 in keysDown) { // Player holding right
-		hero.shoot(direction.RIGHT);
+		hero.shoot(bullets, direction.RIGHT);
 	}
+
 
 	//Move each bullet
 	for (var i = 0; i < bullets.length; i++){
@@ -147,10 +178,36 @@ var update = function (modifier) {
 			bullets[i].x += 280 * modifier;
 		}
 	}
+
+	//Hero only takes damage periodically (to avoid taking a ton of damage really quickly)
+	if (heroInvulnTime > 0){
+		heroInvulnTime -= modifier;
+		if (heroInvulnTime < 0){
+			heroInvulnTime = 0;
+		}
+	}
+
 	//Collision
 	if (bulletCollision()) {
 		++monstersCaught;
 		reset();
+	}
+
+	checkForHeroCollision();
+}
+
+var heroInvulnTime = 0;
+var checkForHeroCollision = function(){
+	if (
+		hero.x <= (monster.x + 32)
+		&& monster.x <= (hero.x + 32)
+		&& hero.y <= (monster.y + 32)
+		&& monster.y <= (hero.y + 32)
+	) {
+		if(heroInvulnTime == 0){
+			hero.health = hero.health - 1;
+			heroInvulnTime = 0.5;
+		}
 	}
 }
 
@@ -158,9 +215,9 @@ var render = function(){
 	if (bgReady) {
 		ctx.drawImage(bgImage, 0, 0);
 	}
-	if (heroReady) {
-		ctx.drawImage(heroImage, hero.x, hero.y);
-	}
+	
+	hero.render(ctx);
+	
 	if (monsterReady) {
 		ctx.drawImage(monsterImage, monster.x, monster.y);	
 	}
@@ -175,7 +232,8 @@ var render = function(){
 	ctx.font = "24px Helvetica";
 	ctx.textAlign = "left";
 	ctx.textBaseline = "top";
-	ctx.fillText("Monsters caught: " + monstersCaught, 32, 32);
+	ctx.fillText("Health: " + hero.health, 32, 32);
+	ctx.fillText("Invuln:" + heroInvulnTime, 220, 32);
 };
 
 var main = function () {
